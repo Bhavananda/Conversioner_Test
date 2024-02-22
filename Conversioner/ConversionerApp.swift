8d74f0ec0cb0b8dd7b17fcba174ce471ddb4d159
Bhavananda <buliekov.vlad@gmail.com>
2024-02-22 13:58:10 +0200

    Initial Commit

---
+++ Conversioner/ConversionerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConversionerApp.swift
+//  Conversioner
+//
+//  Created by Bhavananda Das on 22.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ConversionerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
